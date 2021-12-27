64ab56b5154ecc746e86617c983ad5bf42ae6c38
Autarn Sukonnakorn <one.at.once@gmail.com>
2021-12-27 15:47:54 +0700

    checklogin

--- fe_login_checklogin.php
+++ fe_login_checklogin.php
@@ -1,5 +1,38 @@
 <?php
 require_once('connection.php');
 $_POST = json_decode(file_get_contents("php://input"),true);
+function generateRandomString($length = 20) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 
+$username=$_POST['username'];
+$password=$_POST['password'];
+
+//
+$username="Artzy";
+$password="1234";
+
+$result=$db->count("user",[
+    "username"=>$username,
+    "password"=>$password
+]);
+if($result==0){
+    echo "password incorrect";
+}
+else{
+    $token=generateRandomString();
+    $db->update("user",[
+        "token"=>$token
+    ],[
+        "username"=>$username,
+        "password"=>$password
+    ]);
+    echo $token;
+}
 ?>
